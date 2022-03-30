b9bcc257104abcb7ab98790331b8548e80a90de8
Diego cristaldo <diegocristaldo65@gmail.com>
2022-03-30 22:07:36 +0200

    adicionando projeto de logistica

---
+++ orders.js
@@ -0,0 +1,35 @@
+
+const Orders = [
+{
+   
+    productName:'Mortal Kombat 11',
+    productNumber:'85631',
+    paymentStatus:'Due',
+    shipping: 'Peding'
+},
+{
+    productName:'Street Fighter',
+    productNumber:'3638',
+    paymentStatus:'Refunded',
+    shipping: 'Declined'
+},
+{
+    productName:'The last Of US 2',
+    productNumber:'49347',
+    paymentStatus:'Due',
+    shipping: 'Peding'
+},
+{
+    productName:'GTA 5',
+    productNumber:'96996',
+    paymentStatus:'Paid',
+    shipping: 'Delivered'
+},
+{
+    productName:'Minecraft',
+    productNumber:'22821',
+    paymentStatus:'Paid',
+    shipping: 'Delivered'
+}
+
+]
